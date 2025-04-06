7beee2bcb5a219d6765162846a0038f8b4685d8b
RConradGe <rcgadduang@gmail.com>
2025-04-05 20:55:11 -0700

    wip: added user preview functionality for all users in db

--- src/components/UserPreview/UserPreview.jsx
+++ src/components/UserPreview/UserPreview.jsx
@@ -1,11 +1,40 @@
 import { useState, useEffect } from "react";
+import { userService } from "../../services/userService";
 import * as userService from '../../services/userService';
 
+
 const UserPreview = () => {
-  return (
-    <>
-    {<h1>This shows the user preview</h1>}
-    </>
+  const { user } = useContext(UserContext);
+  const {otherUsers, setOtherUsers} = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const allUsers = await userService.index(); // fetches all users
+        const filteredUsers = allUsers.filter(fetchedUser => fetchedUser._id !== user._id);
+        setOtherUsers(filteredUsers);
+      } catch(error) {
+        console.error(error);
+      }
+    }
+    if (user) fetchUsers()
+  }, [user]);
+  
+  return(
+    <section className="user-profile">
+      {otherUsers.map(otherUser => (
+        <div key={otherUser._id} className="user-preview">
+          <img
+          src={otherUser.profileImg}
+          alt={`${otherUser.username}'s Profile`}
+          className="profile-img"
+          style= {{ width: "80px", height:"80px", borderRadius: "50%" }}/>
+          <p>{otherUser.username}</p>
+          <button>Follower User</button>
+          <button>Unfollow User</button>
+        </div>
+      ))}
+    </section>
   );
 };
 
