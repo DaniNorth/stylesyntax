f7380cd390633be4d3ae4dc2ee99bd9dd6acf8c7
RConradGe <rcgadduang@gmail.com>
2025-04-06 12:41:37 -0700

    wip: added Following and Followers if statements to show number of followers and following number.

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,26 +1,27 @@
 import { useState, useEffect, useContext } from "react";
-
 import { UserContext } from "../../contexts/UserContext";
-
 import * as userService from '../../services/userService';
+
+import "./UserProfile.css"
 
 const UserProfile = () => {
 
   const { user } = useContext(UserContext);
   const [userData, setUserData] = useState(null);
-
+  
   useEffect( () => {
     const fetchUserData = async () => {
       try {
         const data = await userService.getUserById(user._id);
         setUserData(data);
+        console.log(data);
       } catch (error) {
         console.error(error);
       };
     };
 
-    if (user) fetchUserData;
-  }, []);
+    if (user) fetchUserData();
+  }, [user]);
 
   return userData ? (
     <main className="user-profile">
@@ -29,10 +30,17 @@ const UserProfile = () => {
         className='profile-img'
         style={{ width: "80px", height: "80px", borderRadius: "50%" }}
       />
-      <p>Welcome, {userData.username}</p>
-      <p>Following: {userData.following}</p>
-      <p>Followers: {userData.followers}</p>
-      <p>Quiz Results: {userData.quizResults}</p>
+      <p> Button for uploading profile picture goes here</p>
+      <p>Welcome, {userData.user.username}</p>
+      <p>Following: {" "}
+        {userData.user.following.length > 0 ?
+        userData.user.following.length
+        : " You are not following anyone. Follow a few very stylish StyleSyntax users."}</p>
+      <p>Followers: {" "}
+        {userData.user.followers.length > 0 ?
+        userData.user.following.length
+        : "You have 0 followers. Go show off you style!"}</p>
+      <p>Quiz Results: {userData.user.quizResults}</p>
     </main>
   ) : (
     <p>Loading...</p>
