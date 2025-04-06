47476bfebcdf6e722a5f24ce072dd59221108f9e
Angellsworth <angellsworth@msn.com>
2025-04-05 21:01:59 -0600

    wip: updated handleHomeClick to go to landing

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
   const navigate = useNavigate();
   
   const handleHomeClick = () => {
-    navigate('/');
+    navigate('/landing');
   }
   const handleSignOut = () => {
     localStorage.removeItem('token');
