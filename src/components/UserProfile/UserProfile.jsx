43eb22e29a616861b9da8e99c9b393fc6672fca3
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-06 17:03:06 -0700

    wip: updated profile to display folders and OutfitCards with scroll function

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import * as userService from '../../services/userService';
 
+import OutfitCard from "../OutfitCard/OutfitCard";
 import "./UserProfile.css"
 
 const UserProfile = () => {
@@ -41,6 +42,18 @@ const UserProfile = () => {
         userData.user.following.length
         : "You have 0 followers. Go show off you style!"}</p>
       <p>Quiz Results: {userData.user.quizResults}</p>
+
+      {/* Added folder */}
+      {userData.user.folders?.length > 0 && userData.user.folders.map(folder => (
+        <section key={folder._id} className="folder-section">
+          <h3>{folder.title}</h3>
+          <div className="scrolling-folder">
+            {folder.outfits?.map(outfit => (
+              <OutfitCard key={outfit._id} outfit={outfit} />
+            ))}
+          </div>
+        </section>
+      ))}
     </main>
   ) : (
     <p>Loading...</p>
