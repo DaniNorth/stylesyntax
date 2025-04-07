8403c3f28426674064c4b5717fe95dd012a159f1
RConradGe <rcgadduang@gmail.com>
2025-04-07 14:03:11 -0700

    wip: added UserUpdateForm component

--- src/components/UserUpdateForm/UserUpdateForm.jsx
+++ src/components/UserUpdateForm/UserUpdateForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
 import * as userService from '../../services/userService';
@@ -6,29 +6,69 @@ import * as userService from '../../services/userService';
 
 const UserUpdateForm = () => {
 
-  const { user, setUser } = useState(UserContext);
-  const [userData, setUserData] = useState(null);
+  const { user, setUser } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    username: user.username,
+    email: user.email
+  });
+
+  const handleChange = (evt) => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    try {
+      const updatedUser = await userService.updateUser(user._id, formData);
+      setUser(updatedUser);
+      console.log(formData.username);
+      console.log("updated username and email!")
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  useEffect( () => {
+    const fetchUser = async () => {
+      const userData = await userService.getUserById(user._id);
+      setFormData(userData);
+    }
+    if (user) fetchUser();
+  }, [user]);
 
 
 
   return(
     <main>
-      <h1>Edit User</h1>
-      <form>
+      <h1>Edit {formData.username}</h1>
+      <form className="user-update-form" onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
         <input
           required
           type="text"
           name="username"
           id="username"
-        />
+          value={formData.username}
+          onChange={handleChange}
+          
+          />
         <label htmlFor="email">Email:</label>
         <input
           required 
           type="email"
           name="email"
           id="email"
+          // value={formData.email}
+          onChange={handleChange}
         />
+        
+        <button 
+        type="submit"
+        disabled={!formData.username || !formData.email}>
+          SUBMIT
+        </button>
+
       </form>
     </main>
   )
