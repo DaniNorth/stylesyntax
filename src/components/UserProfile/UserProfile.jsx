459308aa662db7fb28a4865040f4db49fc28459f
RConradGe <rcgadduang@gmail.com>
2025-04-07 00:23:51 -0700

    wip: added userProfile image upload

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
-import * as userService from '../../services/userService';
+import * as userService from "../../services/userService";
 
 import OutfitCard from "../OutfitCard/OutfitCard";
 import FolderModal from "../FolderModal/FolderModal";
-import "./UserProfile.css"
+import "./UserProfile.css";
 
 const UserProfile = () => {
-
   const { user } = useContext(UserContext);
   const [userData, setUserData] = useState(null);
   const [showModal, setShowModal] = useState(false);
-  
-  useEffect( () => {
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {
     const fetchUserData = async () => {
       try {
         const data = await userService.getUserById(user._id);
@@ -20,30 +20,67 @@ const UserProfile = () => {
         console.log(data);
       } catch (error) {
         console.error(error);
-      };
+      }
     };
 
     if (user) fetchUserData();
   }, [user]);
 
+  const handleImageUpload = async (evt) => {
+    evt.preventDefault();
+
+    if (!file) {
+      alert("Please select a file first.");
+      return;
+    }
+
+    try {
+      const data = await userService.uploadProfilePic(user._id, file);
+      setUserData((prevData) => ({
+        ...prevData,
+        profileImg: data.profileImg,
+      }));
+
+      setUser((prevUser) => ({
+        ...prevUser,
+        profileImg: data.profileImg,
+      }));
+
+      console.log("Picture Uploaded Successfully", data);
+    } catch (err) {
+      console.error("Upload error:", err.message);
+    }
+  };
+
   return userData ? (
     <main className="user-profile">
       <img
         src={userData.profileImg}
-        className='profile-img'
+        className="profile-img"
         alt="User profile"
         style={{ width: "80px", height: "80px", borderRadius: "50%" }}
       />
-      <p> Button for uploading profile picture goes here</p>
+      <form onSubmit={handleImageUpload}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <button type="submit">Upload Profile Picture</button>
+      </form>
       <p>Welcome, {userData.user.username}</p>
-      <p>Following: {" "}
-        {userData.user.following.length > 0 ?
-        userData.user.following.length
-        : " You are not following anyone. Follow a few very stylish StyleSyntax users."}</p>
-      <p>Followers: {" "}
-        {userData.user.followers.length > 0 ?
-        userData.user.following.length
-        : "You have 0 followers. Go show off you style!"}</p>
+      <p>
+        Following:{" "}
+        {userData.user.following.length > 0
+          ? userData.user.following.length
+          : " You are not following anyone. Follow a few very stylish StyleSyntax users."}
+      </p>
+      <p>
+        Followers:{" "}
+        {userData.user.followers.length > 0
+          ? userData.user.followers.length
+          : "You have 0 followers. Go show off your style!"}
+      </p>
       <p>Quiz Results: {userData.user.quizResults}</p>
       <button
         className="manage-folders-button"
@@ -52,18 +89,19 @@ const UserProfile = () => {
         Manage Folders
       </button>
 
-      {/* Added folder */}
-      {userData.user.folders?.length > 0 && userData.user.folders.map(folder => (
-        <section key={folder._id} className="folder-section">
-          <h3>{folder.title}</h3>
-          <div className="scrolling-folder">
-            {folder.outfits?.map(outfit => (
-              <OutfitCard key={outfit._id} outfit={outfit} />
-            ))}
-          </div>
-        </section>
-      ))}
-    {showModal && <FolderModal onClose={() => setShowModal(false)} />}
+      {userData.user.folders?.length > 0 &&
+        userData.user.folders.map((folder) => (
+          <section key={folder._id} className="folder-section">
+            <h3>{folder.title}</h3>
+            <div className="scrolling-folder">
+              {folder.outfits?.map((outfit) => (
+                <OutfitCard key={outfit._id} outfit={outfit} />
+              ))}
+            </div>
+          </section>
+        ))}
+
+      {showModal && <FolderModal onClose={() => setShowModal(false)} />}
     </main>
   ) : (
     <p>Loading...</p>
