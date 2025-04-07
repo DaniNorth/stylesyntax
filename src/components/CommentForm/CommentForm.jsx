3a9216d0fa13943c3076800308fe52a437caf8e9
Angellsworth <angellsworth@msn.com>
2025-04-06 19:03:52 -0600

    wip: updated commentForm added classnames

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react';
+import {
+    useState
+}
 
-const CommentForm = (props) => {
-    // const [formData, setFormData] = useState({text: ''});
-    const [formData, setFormData] = useState({content: ''});
-    const handleChange = (evt) => {
-        setFormData({
-            ...formData, 
-            [evt.target.name]: evt.target.value
-        });
-    };
-    const handleSubmit = (evt) => {
+from 'react';
+import './CommentForm.css';
+
+const CommentForm=(props)=> {
+
+    const [formData,
+    setFormData]=useState( {
+            content: ''
+        }
+
+    );
+
+    const handleChange=(evt)=> {
+        setFormData( {
+                ...formData,
+                [evt.target.name]: evt.target.value
+            }
+
+        );
+    }
+
+    ;
+
+    const handleSubmit=(evt)=> {
         evt.preventDefault();
         props.handleAddComment(formData);
-        setFormData({text: ''});
-    };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="text-input">Your Comment</label>
-            <textarea 
-                required
-                type="text"
-                // name="text" 
-                name="content" 
-                id="text-input"
-                value={formData.text}
-                onChange={handleChange} 
-            />
-            <button type="submit">SUBMIT COMMENT</button>
-        </form>
-    );
-};
+        setFormData( {
+                text: ''
+            }
+
+        );
+    }
+
+    ;
+
+    return (<form onSubmit= {
+            handleSubmit
+        }
+
+        className="comment-form"> <label htmlFor="text-input">Your Comment</label> <textarea required type="text"
+        // name="text" 
+        name="content"
+        id="text-input"
+
+        value= {
+            formData.text
+        }
+
+        onChange= {
+            handleChange
+        }
+
+        /> <button type="submit">SUBMIT COMMENT</button> </form>);
+}
+
+;
 
 export default CommentForm;
