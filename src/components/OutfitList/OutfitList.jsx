b55751dbdbb9ee0c9442140e0c51d1ef0f868470
Angellsworth <angellsworth@msn.com>
2025-04-08 14:03:46 -0600

    wip: adding a filter modal to outfitlist to add filtering of outfits

--- src/components/OutfitList/OutfitList.jsx
+++ src/components/OutfitList/OutfitList.jsx
@@ -1,19 +1,30 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react'
+import { Link } from 'react-router';
 import './OutfitList.css';
 import OutfitCard from '../OutfitCard/OutfitCard';
+import FilterOutfitsModal from '../FilterOutfitsModal/FilterOutfitsModal';
 
 const OutfitList = ({ outfits }) => {
+  const [showFilter, setShowFilter] = useState(false)
+
   return (
     <main className="outfit-list">
+      <button className='open-filter-button' 
+      onClick={() => setShowFilter(true)}>
+        Filter
+      </button>
+      {showFilter && (
+        <FilterOutfitsModal onClose={() => setShowFilter(false)} />
+      )}
+      <div className="outfit-list-grid">
       {outfits.length === 0 ? (
         <p>No outfits to display yet!</p>
       ) : (
-        <div className="outfit-list-grid">
-          {outfits.map((outfit) => (
+          outfits.map((outfit) => (
             <OutfitCard key={outfit._id} outfit={outfit} />
-          ))}
-        </div>
-      )}
+          ))
+        )}
+      </div>
     </main>
   );
 };
