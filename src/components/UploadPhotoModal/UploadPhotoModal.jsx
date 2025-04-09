f2f8b71294e7928d85111333aefdae15b3c7da30
RConradGe <rcgadduang@gmail.com>
2025-04-09 08:01:09 -0700

    wip: added UploadPhotoModal for changing profile photos

---
+++ src/components/UploadPhotoModal/UploadPhotoModal.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import * as userService from "../../services/userService";
+
+import "./UploadPhotoModal.css";
+
+const UploadPhotoModal = ({ onClose }) => {
+  const { user, setUser } = useContext(UserContext);
+  const [file, setFile] = useState(null);
+  const [isPhotoUploaded, setIsPhotoUploaded] = useState(false);
+
+  const handleFileChange = (evt) => {
+    setFile(evt.target.files[0]);
+  };
+
+  const handleImageUpload = async (evt) => {
+    evt.preventDefault();
+
+    if (!file || !user) {
+      alert("Please select a file first.");
+      return;
+    }
+
+    try {
+      const updatedUser = await userService.uploadProfilePic(user._id, file);
+      setUser((prevUser) => ({
+        ...prevUser,
+        profileImg: updatedUser.profileImg,
+      }));
+      console.log("Picture Uploaded Successfully", updatedUser);
+
+      setIsPhotoUploaded(true);
+
+      setTimeout(() => {
+        onClose();
+      }, 1250);
+    } catch (err) {
+      console.error("Upload error:", err.message);
+    }
+  };
+
+  return (
+    <div className="upload-photo-modal-backdrop">
+      <div className="upload-photo-modal">
+        <button className="upload-photo-close-button" onClick={onClose}>
+          ×
+        </button>
+        <h2>Upload Profile Photo</h2>
+        {isPhotoUploaded && <p>Photo Uploaded</p>}
+        <form className="upload-photo-form" onSubmit={handleImageUpload}>
+          <input
+            type="file"
+            accept="image/*"
+            id="profile-upload"
+            onChange={handleFileChange}
+          />
+          <button className="upload-photo-submit-button" type="submit">
+            Upload Profile Photo
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UploadPhotoModal;
