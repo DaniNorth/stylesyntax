de231d67ef4b3c20b2702beab9eabc9f25b8e692
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-04-08 19:22:39 -0700

    wip: updated the navbar to route to quiz properly

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Link, useNavigate  } from 'react-router';
+import { Link, useNavigate  } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
 import logo from '../../assets/StyleSyntaxLogo.svg';
 import './NavBar.css'
